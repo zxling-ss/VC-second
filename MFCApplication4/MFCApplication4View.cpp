bcf6c31b71353b97c6c2c6048238e730aeb6d223
zxling-ss <57402329+zxling-ss@users.noreply.github.com>
2020-03-17 21:11:52 +0800

    Add files via upload

---
+++ MFCApplication4/MFCApplication4View.cpp
@@ -0,0 +1,97 @@
+﻿
+// MFCApplication4View.cpp: CMFCApplication4View 类的实现
+//
+
+#include "pch.h"
+#include "framework.h"
+// SHARED_HANDLERS 可以在实现预览、缩略图和搜索筛选器句柄的
+// ATL 项目中进行定义，并允许与该项目共享文档代码。
+#ifndef SHARED_HANDLERS
+#include "MFCApplication4.h"
+#endif
+
+#include "MFCApplication4Doc.h"
+#include "MFCApplication4View.h"
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+
+// CMFCApplication4View
+
+IMPLEMENT_DYNCREATE(CMFCApplication4View, CView)
+
+BEGIN_MESSAGE_MAP(CMFCApplication4View, CView)
+	ON_WM_CONTEXTMENU()
+	ON_WM_RBUTTONUP()
+END_MESSAGE_MAP()
+
+// CMFCApplication4View 构造/析构
+
+CMFCApplication4View::CMFCApplication4View() noexcept
+{
+	// TODO: 在此处添加构造代码
+
+}
+
+CMFCApplication4View::~CMFCApplication4View()
+{
+}
+
+BOOL CMFCApplication4View::PreCreateWindow(CREATESTRUCT& cs)
+{
+	// TODO: 在此处通过修改
+	//  CREATESTRUCT cs 来修改窗口类或样式
+
+	return CView::PreCreateWindow(cs);
+}
+
+// CMFCApplication4View 绘图
+
+void CMFCApplication4View::OnDraw(CDC* pDC)
+{
+	CMFCApplication4Doc* pDoc = GetDocument();
+	ASSERT_VALID(pDoc);
+	if (!pDoc)
+		return;
+	// TODO: 在此处为本机数据添加绘制代码
+	pDC->Rectangle(pDoc->m_tagRec);
+}
+
+void CMFCApplication4View::OnRButtonUp(UINT /* nFlags */, CPoint point)
+{
+	ClientToScreen(&point);
+	OnContextMenu(this, point);
+}
+
+void CMFCApplication4View::OnContextMenu(CWnd* /* pWnd */, CPoint point)
+{
+#ifndef SHARED_HANDLERS
+	theApp.GetContextMenuManager()->ShowPopupMenu(IDR_POPUP_EDIT, point.x, point.y, this, TRUE);
+#endif
+}
+
+
+// CMFCApplication4View 诊断
+
+#ifdef _DEBUG
+void CMFCApplication4View::AssertValid() const
+{
+	CView::AssertValid();
+}
+
+void CMFCApplication4View::Dump(CDumpContext& dc) const
+{
+	CView::Dump(dc);
+}
+
+CMFCApplication4Doc* CMFCApplication4View::GetDocument() const // 非调试版本是内联的
+{
+	ASSERT(m_pDocument->IsKindOf(RUNTIME_CLASS(CMFCApplication4Doc)));
+	return (CMFCApplication4Doc*)m_pDocument;
+}
+#endif //_DEBUG
+
+
+// CMFCApplication4View 消息处理程序
---
+++ MFCApplication4/MFCApplication4View.h
@@ -0,0 +1,49 @@
+﻿
+// MFCApplication4View.h: CMFCApplication4View 类的接口
+//
+
+#pragma once
+
+
+class CMFCApplication4View : public CView
+{
+protected: // 仅从序列化创建
+	CMFCApplication4View() noexcept;
+	DECLARE_DYNCREATE(CMFCApplication4View)
+
+// 特性
+public:
+	CMFCApplication4Doc* GetDocument() const;
+
+// 操作
+public:
+
+// 重写
+public:
+	virtual void OnDraw(CDC* pDC);  // 重写以绘制该视图
+	virtual BOOL PreCreateWindow(CREATESTRUCT& cs);
+protected:
+
+// 实现
+public:
+	virtual ~CMFCApplication4View();
+#ifdef _DEBUG
+	virtual void AssertValid() const;
+	virtual void Dump(CDumpContext& dc) const;
+#endif
+
+protected:
+
+// 生成的消息映射函数
+protected:
+	afx_msg void OnFilePrintPreview();
+	afx_msg void OnRButtonUp(UINT nFlags, CPoint point);
+	afx_msg void OnContextMenu(CWnd* pWnd, CPoint point);
+	DECLARE_MESSAGE_MAP()
+};
+
+#ifndef _DEBUG  // MFCApplication4View.cpp 中的调试版本
+inline CMFCApplication4Doc* CMFCApplication4View::GetDocument() const
+   { return reinterpret_cast<CMFCApplication4Doc*>(m_pDocument); }
+#endif
+
